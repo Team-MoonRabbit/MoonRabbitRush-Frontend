aef7e111d272db0a25a9aaaa8dec6952ca10e64d
enbraining <me@bricn.net>
2025-10-04 23:09:53 +0900

    update height

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.className} antialiased flex bg-gray-400`}>
-        <div className="max-w-[430px] relative w-full mx-auto bg-blue-950 h-screen flex flex-col">
+        <div className="max-w-[430px] relative w-full mx-auto bg-blue-950 h-[100dvh] flex flex-col">
           <Toaster position="top-right" />
           <div className="flex mb-auto py-2 h-min absolute w-full z-10">
             <div className="mx-auto flex items-center gap-x-1">
