f54819e32ba66b74cf7d7dbc7cdd74218c0e4b13
enbraining <me@bricn.net>
2025-10-07 00:58:39 +0900

    update login logic

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import GoogleLoginButton from "@/components/google-login-button";
-import { redirect, useRouter } from "next/navigation";
+import { redirect, usePathname, useRouter } from "next/navigation";
 import { useCallback, useEffect } from "react";
 import { toast } from "sonner";
 
-export default function Page() {
+export default function Page({
+  params,
+}: {
+  params: Promise<{ next?: string }>;
+}) {
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchParams = async () => {
+      const { next } = await params;
+      if (next) {
+        router.push(next);
+      }
+    };
+
+    fetchParams();
+  }, []);
 
   const onLogin = useCallback(async () => {
     const response = await fetch(
