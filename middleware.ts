c40bb3d1fcdb5714b031949b76e8b01ac4a3b9cf
enbraining <me@bricn.net>
2025-10-07 00:55:50 +0900

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -62,6 +62,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!error|api/auth/callback|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!error|api/auth/callback|auth/login|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
